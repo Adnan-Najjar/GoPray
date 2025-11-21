65e87d134a23fe37ab334c1aaebb723726b399e2
Adnan Najjar <adnan.najjar1@gmail.com>
2025-11-21 11:18:05 +0400

    Only use the prayer time not the before or after for next_prayer

--- main.go
+++ main.go
@@ -327,7 +327,13 @@ func getPrayerDurations(prayer_times PrayerTimes) ([]PrayerDuration, error) {
 		return prayer_durations[i].Duration < prayer_durations[j].Duration
 	})
 	if len(prayer_durations) > 0 {
-		next_prayer = prayer_durations[0]
+		for _, pd := range prayer_durations {
+			// Only use the prayer time not the before or after
+			if pd.state == "at" {
+				next_prayer = pd
+				break
+			}
+		}
 	}
 
 	return prayer_durations, nil
